d6cc6ae90ae757a66a9500aee47dd4dab97d9fde
vinnietito <vincentnjuguna69@gmail.com>
2025-10-31 16:43:50 +0300

    Commits

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import { assets } from '../assets/assets'
+import { useClerk, UserButton, useUser } from '@clerk/clerk-react'
 
 const Navbar = () => {
+
+    const {openSignIn} = useClerk();
+
   return (
     <div className='shadow-md w-full fixed top-0 left-0 bg-white px-4 py-2 flex justify-between items-center'>
       <div className='container mx-auto flex justify-between items-center'>
         <img src={assets.logo} alt="" />
         <div className='space-x-4 flex gap-4 max-sm:text-xs'>
             <button className='text-gray-600'>Recruiter Login</button>
-            <button className='bg-blue-600 text-white px-6 sm:px-9 py-2 rounded-full'>Login</button>
+            <button onClick={ e => openSignIn()} className='bg-blue-600 text-white px-6 sm:px-9 py-2 rounded-full'>Login</button>
         </div>
       </div>
     </div>
