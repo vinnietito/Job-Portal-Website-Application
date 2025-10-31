e8d757673b3137fe4a4817545ef2bd15a7641490
vinnietito <vincentnjuguna69@gmail.com>
2025-10-31 06:42:25 +0300

    Commits

---
+++ client/src/pages/Applications.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Applications = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Applications
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      Rada G haha
+    </div>
+  )
+}
+
+export default Home
