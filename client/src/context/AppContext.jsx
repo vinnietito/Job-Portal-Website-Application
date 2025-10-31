365ec2888d4deb99f8dcabe28d9adeb24e650396
vinnietito <vincentnjuguna69@gmail.com>
2025-10-31 20:31:27 +0300

    Commits

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -3,11 +3,20 @@ import React, { createContext, useState } from "react";
 export const AppContext = createContext();
 
 export const AppContextProvider = ({ children }) => {
+
+  const [searchFilter, setSearchFilter] = useState({
+    title: '',
+    location: ''
+  })
+
+  const [isSearched, setIsSearched] = useState(false)
+
   const [user, setUser] = useState(null);
 
   const value = {
-    user,
-    setUser,
+    user, setUser,
+    searchFilter, setSearchFilter,
+    isSearched, setIsSearched,
   };
 
   return (
