7fd17df9fb0bf576ecbbb57e9dcd3748a3f9f953
vinnietito <vincentnjuguna69@gmail.com>
2025-10-31 17:10:23 +0300

    Commits

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,22 +1,40 @@
-import React from 'react'
-import { assets } from '../assets/assets'
-import { useClerk, UserButton, useUser } from '@clerk/clerk-react'
+import React from "react";
+import { assets } from "../assets/assets";
+import {
+  SignedIn,
+  SignedOut,
+  UserButton,
+  useClerk
+} from "@clerk/clerk-react";
 
 const Navbar = () => {
-
-    const {openSignIn} = useClerk();
+  const { openSignIn } = useClerk();
 
   return (
-    <div className='shadow-md w-full fixed top-0 left-0 bg-white px-4 py-2 flex justify-between items-center'>
-      <div className='container mx-auto flex justify-between items-center'>
-        <img src={assets.logo} alt="" />
-        <div className='space-x-4 flex gap-4 max-sm:text-xs'>
-            <button className='text-gray-600'>Recruiter Login</button>
-            <button onClick={ e => openSignIn()} className='bg-blue-600 text-white px-6 sm:px-9 py-2 rounded-full'>Login</button>
+    <header className="shadow-md w-full fixed top-0 left-0 bg-white px-4 py-2">
+      <div className="container mx-auto flex justify-between items-center">
+        <img src={assets.logo} alt="logo" className="h-10" />
+
+        <div className="flex items-center gap-4">
+          {/* When user is NOT signed in: show recruiter + sign in */}
+          <SignedOut>
+            <button className="text-gray-600">Recruiter Login</button>
+            <button
+              onClick={() => openSignIn({})}
+              className="bg-blue-600 text-white px-6 sm:px-9 py-2 rounded-full"
+            >
+              Login
+            </button>
+          </SignedOut>
+
+          {/* When user IS signed in: show profile UserButton */}
+          <SignedIn>
+            <UserButton afterSignOutUrl="/" />
+          </SignedIn>
         </div>
       </div>
-    </div>
-  )
-}
+    </header>
+  );
+};
 
-export default Navbar
+export default Navbar;
