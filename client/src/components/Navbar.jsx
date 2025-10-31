c71648d09d0a694505d78c073e8933a6dadf3cf9
vinnietito <vincentnjuguna69@gmail.com>
2025-10-31 07:14:43 +0300

    Commits

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,9 +3,13 @@ import { assets } from '../assets/assets'
 
 const Navbar = () => {
   return (
-    <div>
-      <div>
+    <div className='shadow-md w-full fixed top-0 left-0 bg-white px-4 py-2 flex justify-between items-center'>
+      <div className='container mx-auto flex justify-between items-center'>
         <img src={assets.logo} alt="" />
+        <div className='space-x-4 flex gap-4 max-sm:text-xs'>
+            <button className='text-gray-600'>Recruiter Login</button>
+            <button className='bg-blue-600 text-white px-6 sm:px-9 py-2 rounded-full'>Login</button>
+        </div>
       </div>
     </div>
   )
