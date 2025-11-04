c9fb8798da8db16ee7dc573551df9ec1591b3d90
vinnietito <vincentnjuguna69@gmail.com>
2025-11-04 10:20:29 +0300

    commits

--- client/src/components/JobCard.jsx
+++ client/src/components/JobCard.jsx
@@ -2,24 +2,24 @@ import React from 'react'
 import { assets } from '../assets/assets'
 
 const JobCard = ({ job }) => {
-  return (
-    <div>
-      <div>
-        {/* Job Card */}
-        <img src={assets.company_icon} alt="" />
-      </div>
-      <h4>{job.title}</h4>
-      <div>
-        <span>{job.location}</span>
-        <span>{job.level}</span>
-      </div>
-      <p dangerouslySetInnerHTML={{__html:job.description.slice(0,150)}}></p>
-      <div>
-        <button>Apply now</button>
-        <button>Learn more</button>
-      </div>
-    </div>
-  )
+    return (
+        <div className='border p-6 shadow rounded'>
+            <div className='flex justify-between items-center'>
+                {/* Job Card */}
+                <img className='h-8' src={assets.company_icon} alt="" />
+            </div>
+            <h4 className='font-medium text-xl mt-2'>{job.title}</h4>
+            <div className='flex items-center gap-3 mt-2 text-xs'>
+                <span className='bg-blue-50 border border-blue-200 px-4 py-1.5 rounded'>{job.location}</span>
+                <span className='bg-red-50 border border-red-200 px-4 py-1.5 rounded'>{job.level}</span>
+            </div>
+            <p className='text-gray-500 text-sm mt-4' dangerouslySetInnerHTML={{ __html: job.description.slice(0, 150) }}></p>
+            <div className='mt-4  flex gap-4 text-sm'>
+                <button className='bg-blue-600 text-white px-4 py-2 rounded'>Apply now</button>
+                <button className='text-gray-500 border border-gray-500 rounded px-4 py-2'>Learn more</button>
+            </div>
+        </div>
+    )
 }
 
 export default JobCard
