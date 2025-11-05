97d397df2727b377309398d41cd9437379a71be8
vinnietito <vincentnjuguna69@gmail.com>
2025-11-05 12:21:02 +0300

    commits

--- client/src/components/AppDownload.jsx
+++ client/src/components/AppDownload.jsx
@@ -4,19 +4,19 @@ import { assets } from '../assets/assets'
 const AppDownload = () => {
   return (
     <div className='container px-4 2xl:px-20 mx-auto my-20'>
-        <div>
+        <div className='relative bg-gradient-to-r from-violet-50 to-purple-50 p-12 sm:p-24 lg:p-32 rounded-lg'>
             <div>
-                <h1>Download Mobile App for Better Exprience </h1>
-                <div>
-                    <a href="">
-                        <img src={assets.play_store} alt="App Store" />
+                <h1 className='text-2xl sm:text-4xl font-bold mb-8 max-w-md'>Download Mobile App for Better Exprience </h1>
+                <div className='flex gap-4'>
+                    <a href="#" className='inline-block'>
+                        <img className='h-12' src={assets.play_store} alt="App Store" />
                     </a>
-                    <a href="">
-                        <img src={assets.app_store} alt="Play Store" />
+                    <a href="#" className='inline-block'>
+                        <img className='h-12' src={assets.app_store} alt="Play Store" />
                     </a>
                 </div>
             </div>
-            <img src={assets.app_main_img} alt="" />
+            <img className='absolute w-80 right-0 bottom-0 mr-32 max-lg:hidden' src={assets.app_main_img} alt="" />
         </div>
       
     </div>
