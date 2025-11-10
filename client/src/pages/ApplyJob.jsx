62339e35acc87b4e8f25091893eeb9c4d13f8de1
vinnietito <vincentnjuguna69@gmail.com>
2025-11-11 00:01:27 +0300

    commits

--- client/src/pages/ApplyJob.jsx
+++ client/src/pages/ApplyJob.jsx
@@ -66,15 +66,16 @@ const ApplyJob = () => {
 
             <div className='flex flex-col justify-center text-end text-sm max-md:mx-auto max-md:text-center'>
               <button className='bg-blue-600 p-2.5 px-10 text-white rounded'>Apply Now</button>
-              <p className='mt-1 text-gray-600'>Posted{moment(JobData.date).fromNow()}</p>
+              <p className='mt-1 text-gray-600'>Posted {moment(JobData.date).fromNow()}</p>
             </div>
 
           </div>
 
-        <div>
-          <div>
-            <h2>Job description</h2>
-            <div dangerouslySetInnerHTML={{__html:JobData.description}}></div>
+        <div className='flex flex-col lg:flex-row justify-between items-start'>
+          <div className='w-full lg:w-2/3'>
+            <h2 className='font-bold text-2xl mb-4'>Job description</h2>
+            <div className='rich-text' dangerouslySetInnerHTML={{__html:JobData.description}}></div>
+            <button className='bg-blue-600 p-2.5 px-10 text-white rounded mt-10'>Apply Now</button>
           </div>
         </div>
 
