e92effd6ed7917afbb9b1be91480baca232eec76
vinnietito <vincentnjuguna69@gmail.com>
2025-11-18 11:27:29 +0300

    Commits

--- client/src/components/RecruiterLogin.jsx
+++ client/src/components/RecruiterLogin.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { assets } from '../assets/assets'
 
 const RecruiterLogin = () => {
 
@@ -14,7 +15,26 @@ const RecruiterLogin = () => {
 
   return (
     <div>
-      Login Popup
+      <form action="">
+        <h1>Recruiter {state}</h1>
+        <p>Welcome back! Please sign in to continue</p>
+        <>
+          <div>
+            <img src={assets.person_icon} alt="" />
+            <input onChange={e => setName(e.target.value)} type="text" value={name} placeholder='Company Name' required/>
+          </div>
+
+          <div>
+            <img src={assets.email_icon} alt="" />
+            <input onChange={e => setEmail(e.target.value)} type="email" value={email} placeholder='Email Id' required/>
+          </div>
+
+          <div>
+            <img src={assets.lock_icon} alt="" />
+            <input onChange={e => setPassword(e.target.value)} type="password" value={password} placeholder='Password' required/>
+          </div>
+        </>
+      </form>
     </div>
   )
 }
