8bb7dab15bfae5deb44e0decc1eb03572cbab499
vinnietito <vincentnjuguna69@gmail.com>
2025-11-18 11:48:04 +0300

    Commits

--- client/src/components/RecruiterLogin.jsx
+++ client/src/components/RecruiterLogin.jsx
@@ -14,26 +14,32 @@ const RecruiterLogin = () => {
 
 
   return (
-    <div>
-      <form action="">
-        <h1>Recruiter {state}</h1>
-        <p>Welcome back! Please sign in to continue</p>
+    <div className='absolute top-0 left-0 right-0 bottom-0 z-10 backdrop-blur-sm bg-black/30 flex justify-center items-center'>
+      <form className='relative bg-white p-10 rounded-xl text-slate-500'>
+        <h1 className='text-center text-2xl text-neutral-700 font-medium'>Recruiter {state}</h1>
+        <p className='text-sm'>Welcome back! Please sign in to continue</p>
         <>
-          <div>
+          <div className='border px-4 py-2 flex items-center gap-2 rounded-full mt-5'>
             <img src={assets.person_icon} alt="" />
-            <input onChange={e => setName(e.target.value)} type="text" value={name} placeholder='Company Name' required/>
+            <input className='outline-none text-sm' onChange={e => setName(e.target.value)} type="text" value={name} placeholder='Company Name' required/>
           </div>
 
-          <div>
+          <div className='border px-4 py-2 flex items-center gap-2 rounded-full mt-5'>
             <img src={assets.email_icon} alt="" />
-            <input onChange={e => setEmail(e.target.value)} type="email" value={email} placeholder='Email Id' required/>
+            <input className='outline-none text-sm' onChange={e => setEmail(e.target.value)} type="email" value={email} placeholder='Email Id' required/>
           </div>
 
-          <div>
+          <div className='border px-4 py-2 flex items-center gap-2 rounded-full mt-5'>
             <img src={assets.lock_icon} alt="" />
-            <input onChange={e => setPassword(e.target.value)} type="password" value={password} placeholder='Password' required/>
+            <input className='outline-none text-sm' onChange={e => setPassword(e.target.value)} type="password" value={password} placeholder='Password' required/>
           </div>
+
+          <p className='text-sm text-blue-600 my-4 cursor-pointer'>Forgot Password</p>
+
         </>
+        <button className='bg-blue-600 text-white px-6 py-2 rounded-full w-full mt-8'>
+          {state === 'Login' ? 'login' : 'create account'} 
+        </button>
       </form>
     </div>
   )
