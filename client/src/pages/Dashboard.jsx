f9939618fd946581e38ae8929043fc9f27cb8102
vinnietito <vincentnjuguna69@gmail.com>
2025-11-24 12:47:46 +0300

    Commits

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -33,18 +33,18 @@ const Dashboard = () => {
         <div className='inline-block min-h-screen border-r-2'>
           <ul className='flex flex-col items-start pt-5 text-gray-800'>
             <NavLink className={({isActive}) => ` flex items-center p-3 sm:px-6 gap-2 w-full hover:bg-gray-100 ${isActive && 'bg-blue-100 border-r-4 border-blue-500'}`} to={'/dashboard/add-job'}>
-                <img src={assets.add_icon} alt="" />
-                <p>Add Job</p>
+                <img className='min-w-4' src={assets.add_icon} alt="" />
+                <p className='max-sm:hidden'>Add Job</p>
             </NavLink>
 
             <NavLink className={({isActive}) => ` flex items-center p-3 sm:px-6 gap-2 w-full hover:bg-gray-100 ${isActive && 'bg-blue-100 border-r-4 border-blue-500'}`} to={'/dashboard/manage-jobs'}>
-                <img src={assets.home_icon} alt="" />
-                <p>Manage Jobs</p>
+                <img className='min-w-4' src={assets.home_icon} alt="" />
+                <p className='max-sm:hidden'>Manage Jobs</p>
             </NavLink>
 
             <NavLink className={({isActive}) => ` flex items-center p-3 sm:px-6 gap-2 w-full hover:bg-gray-100 ${isActive && 'bg-blue-100 border-r-4 border-blue-500'}`} to={'/dashboard/view-applications'}>
-                <img src={assets.person_tick_icon} alt="" />
-                <p>View Applications</p>
+                <img className='min-w-4' src={assets.person_tick_icon} alt="" />
+                <p className='max-sm:hidden'>View Applications</p>
             </NavLink>
           </ul>
         </div>
--- client/src/pages/ManageJobs.jsx
+++ client/src/pages/ManageJobs.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const ManageJobs = () => {
   return (
     <div>
-      
+      hehe
     </div>
   )
 }
