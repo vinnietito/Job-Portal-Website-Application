aedc6f4a99213ed7c669e68c009bba9e932f4072
vinnietito <vincentnjuguna69@gmail.com>
2025-11-24 22:10:12 +0300

    Commits

--- client/src/pages/AddJob.jsx
+++ client/src/pages/AddJob.jsx
@@ -69,11 +69,11 @@ const AddJob = () => {
           
           </div>
           <div>
-            <p>Job Salary</p>
-            <input onChange={e => setSalary(e.target.value)} type="number" placeholder='2500' />
+            <p className='mb-2'>Job Salary</p>
+            <input min={0} className='w-full px-3 py-2 border-2 border-gray-300 rounded sm:w-[120px]' onChange={e => setSalary(e.target.value)} type="number" placeholder='2500' />
           </div>
 
-          <button>ADD</button>
+          <button className='py-3 mt-4 text-white bg-black rounded w-28'>ADD</button>
 
       </form>
     </div>
