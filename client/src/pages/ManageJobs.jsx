3d8a3c15e0307e7b1ee06dcb5341cdacff6697fc
vinnietito <vincentnjuguna69@gmail.com>
2025-11-25 16:08:22 +0300

    Commits

--- client/src/pages/ManageJobs.jsx
+++ client/src/pages/ManageJobs.jsx
@@ -4,25 +4,30 @@ import moment from 'moment'
 
 const ManageJobs = () => {
   return (
-    <div>
-      <div>
-        <table>
+    <div className='container max-w-5xl p-4'>
+      <div className='overflow-x-auto'>
+        <table className='min-w-full bg-white border-gray-200 max-sm:text-sm'>
           <thead>
             <tr>
-              <th>#</th>
-              <th>Job Title</th>
-              <th>Date</th>
-              <th>Location</th>
-              <th>Applicants</th>
-              <th>Visible</th>
+              <th className='py-2 px-4 border-b text-left max-sm:hidden'>#</th>
+              <th className='py-2 px-4 border-b text-left'>Job Title</th>
+              <th className='py-2 px-4 border-b text-left max-sm:hidden'>Date</th>
+              <th className='py-2 px-4 border-b text-left max-sm:hidden'>Location</th>
+              <th className='py-2 px-4 border-b text-center'>Applicants</th>
+              <th className='py-2 px-4 border-b text-left'>Visible</th>
             </tr>
           </thead>
           <tbody>
             {manageJobsData.map((job, index)=>(
-              <tr key={index}>
-                <td>{index+1}</td>
-                <td>{job.title}</td>
-                <td>{moment(job.date)}</td>
+              <tr key={index} className='text-gray-700'>
+                <td className='py-2 px-4 border-4 max-sm:hidden'>{index+1}</td>
+                <td className='py-2 px-4 border-4'>{job.title}</td>
+                <td className='py-2 px-4 border-4 max-sm:hidden'>{moment(job.date).format('ll')}</td>
+                <td className='py-2 px-4 border-4 max-sm:hidden'>{job.location}</td>
+                <td className='py-2 px-4 border-4 text-center'>{job.applicants}</td>
+                <td className='py-2 px-4 border-4'>
+                  <input type="checkbox" />
+                </td>
               </tr>
             ))}
           </tbody>
