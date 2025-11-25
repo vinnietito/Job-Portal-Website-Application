e85dbe9fe48159c05a4d5d0e52d239f275638d0e
vinnietito <vincentnjuguna69@gmail.com>
2025-11-25 15:07:33 +0300

    Commits

--- client/src/pages/ViewApplications.jsx
+++ client/src/pages/ViewApplications.jsx
@@ -3,7 +3,20 @@ import React from 'react'
 const ViewApplications = () => {
   return (
     <div>
-      
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>User Name</th>
+              <th>Job Title</th>
+              <th>Location</th>
+              <th>Resume</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+        </table>
+      </div>
     </div>
   )
 }
