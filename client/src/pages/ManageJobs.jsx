c8b0d8f43ec5ae4ee3a85234e7ba2b398914a614
vinnietito <vincentnjuguna69@gmail.com>
2025-11-25 15:53:23 +0300

    Commits

--- client/src/pages/ManageJobs.jsx
+++ client/src/pages/ManageJobs.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
+import { manageJobsData } from '../assets/assets'
+import moment from 'moment'
 
 const ManageJobs = () => {
   return (
     <div>
-      hehe
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Job Title</th>
+              <th>Date</th>
+              <th>Location</th>
+              <th>Applicants</th>
+              <th>Visible</th>
+            </tr>
+          </thead>
+          <tbody>
+            {manageJobsData.map((job, index)=>(
+              <tr key={index}>
+                <td>{index+1}</td>
+                <td>{job.title}</td>
+                <td>{moment(job.date)}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   )
 }
