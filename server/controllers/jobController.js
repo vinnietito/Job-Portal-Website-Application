2dfca71b6a95dd887d2522ed45296a4cedbe5a00
vinnietito <vincentnjuguna69@gmail.com>
2025-12-19 16:17:34 +0300

    Commits

---
+++ server/controllers/jobController.js
@@ -0,0 +1,14 @@
+
+
+
+
+// Get all jobs
+export const getJobs = async (req, res) => {
+
+
+}
+
+// Get a single job by ID
+export const getJobById = async (req, res) => {
+
+}
---
+++ server/routes/jobRoutes.js
@@ -0,0 +1,10 @@
+import express from 'express'
+
+const router = express.Router()
+
+// Route to get all jobs data
+
+
+// Route to get a single job by ID
+
+export default router;
