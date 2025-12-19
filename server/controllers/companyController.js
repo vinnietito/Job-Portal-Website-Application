59ca4f9849d84bb8bb4745c74dba4e78a3ab4ba2
vinnietito <vincentnjuguna69@gmail.com>
2025-12-19 16:04:04 +0300

    Commits

--- server/controllers/companyController.js
+++ server/controllers/companyController.js
@@ -2,6 +2,7 @@ import Company from "../models/Company.js";
 import bcrypt from "bcrypt"
 import { v2 as cloudinary } from 'cloudinary'
 import generateToken from "../utils/generatetoken.js";
+import Job from "../models/Job.js";
 
 // Register a new company
 export const registerCompany = async (req, res) => {
@@ -93,11 +94,32 @@ export const getCompanyData = async (req, res) => {
 // Post a new job
 export const postJob = async (req, res) => {
 
-    const { title, description, location, salary } = req.body
+    const { title, description, location, salary, level, category } = req.body
 
     const companyId = req.company._id
 
-    console.log(companyId, {title, description, location, salary});
+    try {
+
+        const newJob = new Job({
+            title,
+            description,
+            location,
+            salary,
+            companyId,
+            date: Date.now(),
+            level,
+            category
+        })
+
+        await newJob.save()
+
+        res.json({ success: true, job: newJob })
+        
+    } catch (error) {
+
+        res.json({ success: false, message: error.message })
+        
+    }
 
 }
 
--- server/models/Job.js
+++ server/models/Job.js
@@ -7,10 +7,9 @@ const jobSchema = new mongoose.Schema({
     category: { type: String, required: true},
     level: { type: String, required: true},
     salary: { type: Number, required: true},
-    date: {type:Number, required:true},
-    visible: { type: Boolean, required: true },
+    date: {type:Number, default:true},
+    visible: { type: Boolean, default: true },
     companyId: { type: mongoose.Schema.Types.ObjectId, ref: "Company", required: true }
-
 })
 
 const Job = mongoose.model("Job", jobSchema)
