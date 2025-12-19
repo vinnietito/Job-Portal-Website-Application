db3105b5580457235decdc52d180533ccb5723f6
vinnietito <vincentnjuguna69@gmail.com>
2025-12-19 14:12:40 +0300

    Commits

--- server/routes/companyRoutes.js
+++ server/routes/companyRoutes.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import { changeJobApplicationStatus, changeVisibility, getCompanyData, getCompanyJobApplicants, getCompanyPostedJobs, loginCompany, postJob, registerCompany } from '../controllers/companyController.js'
 import upload from '../config/multer.js'
+import { protectCompany } from '../middleware/authMiddleware.js'
 
 const router = express.Router()
 
@@ -11,21 +12,21 @@ router.post('/register', upload.single('image'), registerCompany)
 router.post('/login', loginCompany)
 
 // Get Company Data
-router.get('/company', getCompanyData)
+router.get('/company', protectCompany, getCompanyData)
 
 // Post a job
-router.post('/post-job', postJob)
+router.post('/post-job', protectCompany, postJob)
 
 // Get Applicants Data of Company
-router.get('/applicants', getCompanyJobApplicants)
+router.get('/applicants', protectCompany, getCompanyJobApplicants)
 
 // Get Company Job List
-router.get('/list-jobs', getCompanyPostedJobs)
+router.get('/list-jobs', protectCompany, getCompanyPostedJobs)
 
 // Change Applications Status
-router.post('/change-status', changeJobApplicationStatus)
+router.post('/change-status', protectCompany, changeJobApplicationStatus)
 
 // Change Applications Visibility
-router.post('/change-visibility', changeVisibility)
+router.post('/change-visibility', protectCompany, changeVisibility)
 
 export default router
