5531067821f773b81fcb449bdc904db7b2309b72
vinnietito <vincentnjuguna69@gmail.com>
2026-01-09 21:02:29 +0300

    It feels good to be back.... first 2026 commit

---
+++ server/controllers/userController.js
@@ -0,0 +1,22 @@
+
+
+
+// Get user Data
+export const getUserData = async(req, res) => {
+
+}
+
+// Apply for a job
+export const applyForJob = async (req, res) => {
+
+}
+
+// Get user applied application
+export const getUserJobApplications = async (req, res) => {
+
+}
+
+// Update user profile (resume)
+export const updateUserResume = async (req, res) => {
+
+}
