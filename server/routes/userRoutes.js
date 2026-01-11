761b3ae2a4223495d8f89f1e97b24974162a3db0
vinnietito <vincentnjuguna69@gmail.com>
2026-01-11 23:53:49 +0300

    Commits

--- server/routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-import { getUserData } from '../controllers/userController.js'
-
-
-const router = express.Router()
-
-// Get user data
-router.get('/user', getUserData)
