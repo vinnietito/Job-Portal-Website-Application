4d227ef6c8c206785ad84d78f38a346c957f1e90
vinnietito <vincentnjuguna69@gmail.com>
2026-02-22 20:42:11 +0300

    Commits

--- server/server.js
+++ server/server.js
@@ -11,6 +11,7 @@ import { clerkWebhooks } from "./controllers/webhooks.js";
 import connectCloudinary from "./config/cloudinary.js";
 import jobRoutes from './routes/jobRoutes.js'
 import userRoutes from './routes/userRoutes.js'
+import {clerkMiddleware} from '@clerk/express'
 
 // Initialize Express
 const app = express();
@@ -22,6 +23,7 @@ await connectCloudinary();
 // Middlewares
 app.use(cors());
 app.use(express.json());
+app.use(clerkMiddleware());
 
 // Routes
 app.get("/", (req, res) => {
