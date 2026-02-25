5d7086dfe738d0c9a970c3a94bcbd9fdc6aa5ba5
vinnietito <vincentnjuguna69@gmail.com>
2026-02-25 11:21:38 +0300

    Commits

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,8 +1,25 @@
-
+import User from "../models/User.js";
 
 
 // Get user Data
 export const getUserData = async(req, res) => {
+
+    const userId = req.auth.userId;
+
+    try {
+
+        const user = await User.findById(userId)
+
+        if(!user) {
+            return res.json({ success: false, message: "User not found" })
+        }
+
+        res.json({ success: true, user })
+        
+    } catch (error) {
+        res.json({success:false, message: error.message})
+        
+    }
 
 }
 
