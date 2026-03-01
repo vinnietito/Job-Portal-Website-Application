f6fafed90e973d81e499d519f74dd3dd2adc7cdd
vinnietito <vincentnjuguna69@gmail.com>
2026-03-01 15:00:41 +0300

    Commits

--- client/src/pages/AddJob.jsx
+++ client/src/pages/AddJob.jsx
@@ -1,126 +1,149 @@
-import React, { useContext, useEffect, useRef, useState } from 'react'
-import Quill from 'quill'
-import { JobCategories, JobLocations } from '../assets/assets';
-import axios from 'axios';
-import { AppContext } from '../context/AppContext';
-import { toast } from 'react-toastify';
+import React, { useContext, useEffect, useRef, useState } from "react";
+import Quill from "quill";
+import { JobCategories, JobLocations } from "../assets/assets";
+import axios from "axios";
+import { AppContext } from "../context/AppContext";
+import { toast } from "react-toastify";
 
 const AddJob = () => {
-
-  const [title, setTitle] = useState('');
-  const [location, setLocation] = useState('Nairobi');
-  const [category, setCategory] = useState('Programming');
-  const [level, setLevel] = useState('Beginner level');
+  const [title, setTitle] = useState("");
+  const [location, setLocation] = useState("Nairobi");
+  const [category, setCategory] = useState("Programming");
+  const [level, setLevel] = useState("Beginner level");
   const [salary, setSalary] = useState(0);
 
-  const editorRef = useRef(null)
-  const quillRef = useRef(null)
+  const editorRef = useRef(null);
+  const quillRef = useRef(null);
 
-  const { backendUrl, companyToken } = useContext(AppContext)
+  const { backendUrl, companyToken } = useContext(AppContext);
 
   const onSubmitHandler = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
 
     try {
+      const description = quillRef.current.root.innerHTML;
 
-      const description = quillRef.current.root.innerHTML
+      const { data } = await axios.post(
+        backendUrl + "/api/company/post-job",
+        {
+          title,
+          description,
+          location,
+          category,
+          level,
+          salary,
+        },
+        {
+          headers: { token: companyToken },
+        },
+      );
 
-      const { data } = await axios.post(backendUrl + '/api/jobs/add',
-      {
-        title,
-        description,
-        location,
-        category,
-        level,
-        salary
-      },
-    {
-      headers: { token: companyToken}
-    })
-
-    if (data.success) {
-      toast.success(data.message)
-      setTitle('')
-      setLocation('Nairobi')
-      setCategory('Programming')
-      setLevel('Beginner level')
-      setSalary(0)
-      quillRef.current.root.innerHTML = ''
-    } else {
-      toast.error(data.message)
-    }
-      
+      if (data.success) {
+        toast.success(data.message);
+        setTitle("");
+        // setLocation('Nairobi')
+        // setCategory('Programming')
+        // setLevel('Beginner level')
+        setSalary(0);
+        quillRef.current.root.innerHTML = "";
+      } else {
+        toast.error(data.message);
+      }
     } catch (error) {
-      
+      toast.error(error.message);
     }
-  }
+  };
 
   useEffect(() => {
     //Initialize quill only once
     if (!quillRef.current && editorRef.current) {
       quillRef.current = new Quill(editorRef.current, {
-        theme: 'snow',
-        })
+        theme: "snow",
+      });
     }
-  }, [])
+  }, []);
 
   return (
     <div>
-      <form onSubmit={onSubmitHandler} className='container flex flex-col items-start w-full gap-3 p-4'>
+      <form
+        onSubmit={onSubmitHandler}
+        className="container flex flex-col items-start w-full gap-3 p-4"
+      >
+        <div className="w-full">
+          <p className="mb-2">Job Title</p>
+          <input
+            className="w-full max-w-lg px-3 py-2 border-2 border-gray-300 rounded"
+            type="text"
+            placeholder="Type here"
+            onChange={(e) => setTitle(e.target.value)}
+            value={title}
+            required
+          />
+        </div>
 
-          <div className='w-full'>
-            <p className='mb-2'>Job Title</p>
-            <input className='w-full max-w-lg px-3 py-2 border-2 border-gray-300 rounded' type="text" placeholder='Type here' onChange={e => setTitle(e.target.value)} value={title} required />
-          </div>
+        <div className="w-full max-w-lg">
+          <p className="my-2">Job Description</p>
+          <div ref={editorRef}></div>
+        </div>
 
-          <div className='w-full max-w-lg'>
-            <p className='my-2'>Job Description</p>
-            <div ref={editorRef}>
-
-            </div>
-          </div>
-
-          <div className='flex flex-col w-full gap-2 sm:flex-row sm:gap-8'>
-
-            <div>
-              <p className='mb-2'>Job Category</p>
-              <select  className='w-full px-3 py-2 border-2 border-gray-300 rounded' onChange={e => setCategory(e.target.value)}>
-                {JobCategories.map((category,index)=>(
-                  <option value={category} key={index}>{category}</option>
-                ))}
-              </select>
-            </div>
-
-            <div>
-              <p className='mb-2'>Job Location</p>
-              <select className='w-full px-3 py-2 border-2 border-gray-300 rounded' onChange={e => setLocation(e.target.value)}>
-                {JobLocations.map((location,index)=>(
-                  <option value={location} key={index}>{location}</option>
-                ))}
-              </select>
-            </div>
-
-            <div>
-              <p className='mb-2'>Job Level</p>
-              <select className='w-full px-3 py-2 border-2 border-gray-300 rounded'  onChange={e => setLevel(e.target.value)}>
-                <option value="Beginner level">Beginner level</option>
-                <option value="Intermediate level">Intermediate level</option>
-                <option value="Senior level">Senior level</option>
-              </select>
-            </div>
-          
-          </div>
+        <div className="flex flex-col w-full gap-2 sm:flex-row sm:gap-8">
           <div>
-            <p className='mb-2'>Job Salary</p>
-            <input min={0} className='w-full px-3 py-2 border-2 border-gray-300 rounded sm:w-[120px]' onChange={e => setSalary(e.target.value)} type="number" placeholder='2500' />
+            <p className="mb-2">Job Category</p>
+            <select
+              className="w-full px-3 py-2 border-2 border-gray-300 rounded"
+              onChange={(e) => setCategory(e.target.value)}
+            >
+              {JobCategories.map((category, index) => (
+                <option value={category} key={index}>
+                  {category}
+                </option>
+              ))}
+            </select>
           </div>
-          
 
-          <button className='py-3 mt-4 text-white bg-black rounded w-28'>ADD</button>
+          <div>
+            <p className="mb-2">Job Location</p>
+            <select
+              className="w-full px-3 py-2 border-2 border-gray-300 rounded"
+              onChange={(e) => setLocation(e.target.value)}
+            >
+              {JobLocations.map((location, index) => (
+                <option value={location} key={index}>
+                  {location}
+                </option>
+              ))}
+            </select>
+          </div>
 
+          <div>
+            <p className="mb-2">Job Level</p>
+            <select
+              className="w-full px-3 py-2 border-2 border-gray-300 rounded"
+              onChange={(e) => setLevel(e.target.value)}
+            >
+              <option value="Beginner level">Beginner level</option>
+              <option value="Intermediate level">Intermediate level</option>
+              <option value="Senior level">Senior level</option>
+            </select>
+          </div>
+        </div>
+        <div>
+          <p className="mb-2">Job Salary</p>
+          <input
+            min={0}
+            className="w-full px-3 py-2 border-2 border-gray-300 rounded sm:w-[120px]"
+            onChange={(e) => setSalary(e.target.value)}
+            type="number"
+            placeholder="2500"
+          />
+        </div>
+
+        <button className="py-3 mt-4 text-white bg-black rounded w-28">
+          ADD
+        </button>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default AddJob
+export default AddJob;
