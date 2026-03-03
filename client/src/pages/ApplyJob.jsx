4e8572a6f74ff7020d3cc435979049e93ed1a687
vinnietito <vincentnjuguna69@gmail.com>
2026-03-03 17:42:22 +0300

    Commits

--- client/src/pages/ApplyJob.jsx
+++ client/src/pages/ApplyJob.jsx
@@ -18,7 +18,7 @@ const ApplyJob = () => {
 
   const [JobData, setJobData] = useState(null)
 
-  const { jobs, backendUrl } = useContext(AppContext)
+  const { jobs, backendUrl, userData, userApplications } = useContext(AppContext)
 
   const fetchJob = async () => {
 
@@ -40,6 +40,20 @@ const ApplyJob = () => {
   }
 
   const applyHandler = async () => {
+    
+    try {
+
+      if (!userData) {
+        return toast.error('Please login to apply for this job')
+      }
+
+      if (!userData.resume) {
+        return toast.error('Please upload your resume to apply for this job')
+      }
+      
+    } catch (error) {
+      
+    }
 
     
   }
@@ -85,7 +99,7 @@ const ApplyJob = () => {
             </div>
 
             <div className='flex flex-col justify-center text-end text-sm max-md:mx-auto max-md:text-center'>
-              <button className='bg-blue-600 p-2.5 px-10 text-white rounded'>Apply Now</button>
+              <button onClick={applyHandler} className='bg-blue-600 p-2.5 px-10 text-white rounded'>Apply Now</button>
               <p className='mt-1 text-gray-600'>Posted {moment(JobData.date).fromNow()}</p>
             </div>
 
@@ -95,7 +109,7 @@ const ApplyJob = () => {
           <div className='w-full lg:w-2/3'>
             <h2 className='font-bold text-2xl mb-4'>Job description</h2>
             <div className='rich-text' dangerouslySetInnerHTML={{__html:JobData.description}}></div>
-            <button className='bg-blue-600 p-2.5 px-10 text-white rounded mt-10'>Apply Now</button>
+            <button onClick={applyHandler} className='bg-blue-600 p-2.5 px-10 text-white rounded mt-10'>Apply Now</button>
           </div>
           <div className='w-full lg:w-1/3 mt-8 lg:mt-0 lg:ml-8 space-y-5'>
             {/* Right Section More Jobs */}
