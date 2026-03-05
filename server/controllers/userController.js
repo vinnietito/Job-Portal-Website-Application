43d97530ad60b0de41301748493a2dc2f6aabcf6
vinnietito <vincentnjuguna69@gmail.com>
2026-03-06 00:02:16 +0300

    Commits

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -87,7 +87,7 @@ export const getUserJobApplications = async (req, res) => {
 export const updateUserResume = async (req, res) => {
     try {
         const userId = req.auth.userId
-        const resumeFile = req.resumeFile
+        const resumeFile = req.file
         const userData = await User.findById(userId)
 
         if (resumeFile) {
