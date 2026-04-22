cf4d71d439b465475bc418e0f712fd9294486b70
vinnietito <vincentnjuguna69@gmail.com>
2026-04-22 08:36:08 +0300

    Commits

--- client/src/pages/Applications.jsx
+++ client/src/pages/Applications.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useState, useEffect } from 'react'
 import Navbar from '../components/Navbar'
 import { assets } from '../assets/assets'
 import moment from 'moment'
@@ -20,7 +20,7 @@ const Applications = () => {
 
   // const [jobsApplied, setJobsApplied] = useState([])
 
-  const { backendUrl, userData, userApplications, fetchUserData } = useContext(AppContext)
+  const { backendUrl, userData, userApplications, fetchUserData, fetchUserApplications } = useContext(AppContext)
 
   const updateResume = async () => {
 
@@ -52,54 +52,62 @@ const Applications = () => {
 
   }
 
+  useEffect(() => {
+
+    if (user) {
+      fetchUserApplications()
+    }
+
+  }, [user])
+
   return (
     <>
       <Navbar />
       <div className='container px-4 min-h-[65vh] 2xl:px-20 mx-auto my-20'>
         <h2 className='text-xl font-semibold'>Your Resume</h2>
-        <div className='flex gap-2 mb-6 mt-3'>
+        <div className='flex gap-2 mt-3 mb-6'>
           {
             isEdit || userData && userData.resume === ""
               ? <>
                 <label className='flex items-center' htmlFor="resumeUpload">
-                  <p className='bg-blue-100 text-blue-600 px-4 py-2 rounded-lg mr-2'>{resume ? resume.name : "Select Resume"}</p>
+                  <p className='px-4 py-2 mr-2 text-blue-600 bg-blue-100 rounded-lg'>{resume ? resume.name : "Select Resume"}</p>
                   <input id='resumeUpload' onChange={e => setResume(e.target.files[0])} accept='application/pdf' type="file" hidden />
                   <img src={assets.profile_upload_icon} alt="" />
                 </label>
-                <button onClick={updateResume} className='bg-green-100 border border-green-400 rounded-lg px-4 py-2'>Save</button>
+                <button onClick={updateResume} className='px-4 py-2 bg-green-100 border border-green-400 rounded-lg'>Save</button>
               </>
               : <div className='flex gap-2'>
-                <a className='bg-blue-100 text-blue-600 px-4 py-2 rounded-lg' href="">
+                <a className='px-4 py-2 text-blue-600 bg-blue-100 rounded-lg' href="">
                   Resume
                 </a>
-                <button onClick={() => setIsEdit(true)} className='text-gray-500 border border-gray-300 rounded-lg px-4 py-2'>
+                <button onClick={() => setIsEdit(true)} className='px-4 py-2 text-gray-500 border border-gray-300 rounded-lg'>
                   Edit
                 </button>
               </div>
           }
         </div>
-        <h2 className='text-xl font-semibold mb-4'>Jobs Applied</h2>
+        <h2 className='mb-4 text-xl font-semibold'>Jobs Applied</h2>
         <table className='min-w-full bg-white border rounded-lg '>
           <thead>
             <tr>
-              <th className='py-3 px-4 border-b text-left'>Company</th>
-              <th className='py-3 px-4 border-b text-left'>Job Title</th>
-              <th className='py-3 px-4 border-b text-left max-sm:hidden'>Location</th>
-              <th className='py-3 px-4 border-b text-left max-sm:hidden'>Date</th>
-              <th className='py-3 px-4 border-b text-left'>Status</th>
+              <th className='px-4 py-3 text-left border-b'>Company</th>
+              <th className='px-4 py-3 text-left border-b'>Job Title</th>
+              <th className='px-4 py-3 text-left border-b max-sm:hidden'>Location</th>
+              <th className='px-4 py-3 text-left border-b max-sm:hidden'>Date</th>
+              <th className='px-4 py-3 text-left border-b'>Status</th>
             </tr>
           </thead>
           <tbody>
             {userApplications.map((job, index) => true ? (
               <tr key={index}>
-                <td className='py-3 px-4 flex items-center gap-2 border-b'>
+                <td className='flex items-center gap-2 px-4 py-3 border-b'>
                   <img className='w-8 h-8' src={job.companyId.image} alt="" />
                   {job.companyId.name}
                 </td>
-                <td className='py-2 px-4 border-b'>{job.jobId.title}</td>
-                <td className='py-2 px-4 border-b max-sm:hidden'>{job.jobId.location}</td>
-                <td className='py-2 px-4 border-b max-sm:hidden'>{moment(job.date).format('ll')}</td>
-                <td className='py-2 px-4 border-b'>
+                <td className='px-4 py-2 border-b'>{job.jobId.title}</td>
+                <td className='px-4 py-2 border-b max-sm:hidden'>{job.jobId.location}</td>
+                <td className='px-4 py-2 border-b max-sm:hidden'>{moment(job.date).format('ll')}</td>
+                <td className='px-4 py-2 border-b'>
                   <span className={`${job.status === 'Accepted' ? 'bg-green-100' : job.status === 'Rejected' ? 'bg-red-100' : 'bg-blue-100'} px-4 py-1.5 rounded`}>
                     {job.status}
                   </span>
